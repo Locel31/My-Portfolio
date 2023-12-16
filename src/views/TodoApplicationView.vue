1d33ffae7dfb7f677459a073bc1146d6c7976152
Locel31 <134920820+Locel31@users.noreply.github.com>
2023-12-17 03:38:28 +0800

    Update TodoApplicationView.vue

--- src/views/TodoApplicationView.vue
+++ src/views/TodoApplicationView.vue
@@ -1,1 +1,17 @@
+<template>
+  <TodoApplicationComponent>
+</template>
 
+<script>
+import TodoApplicationComponent from '@/components/TodoApplicationComponent.vue'
+
+export default {
+    components: {
+        TodoApplicationComponent,
+    }
+}
+</script>
+
+<style>
+
+</style>
