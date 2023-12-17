fb7d5d184b56f96ce0ab2cb03f2798ddefc6ed2e
Locel31 <134920820+Locel31@users.noreply.github.com>
2023-12-17 10:11:41 +0800

    Update MyprojectView.vue update

--- src/views/MyprojectView.vue
+++ src/views/MyprojectView.vue
@@ -1,1 +1,17 @@
+<template>
+  
+</template>
 
+<script>
+import LoginComponent from '@/components/LoginComponent.vue'
+
+export default {
+    components: {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
