fc7db77d46da42b4560bf5079c41de04b44b6212
Locel31 <134920820+Locel31@users.noreply.github.com>
2023-12-17 10:14:54 +0800

    Update MyprojectView.vue test

--- src/views/MyprojectView.vue
+++ src/views/MyprojectView.vue
@@ -1,10 +1,28 @@
 <template>
-  
+  <div class="row">
+        <div class="col-12">
+          <h2 class="fw-bold" style="color:#35e788">My Project</h2>
+        </div>
+        <div class="col-12 ">
+          <a href="/my-project/todo-application">
+            <div class="card">
+             <img src="@/assets/img/todo.JPG.jpg" class="img-fluid" alt="">
+              <span class="py-2 px-4"> Todo Application</span>
+            </div>
+            </a>
+        </div>
+        <div class="col-12 mt-4">
+          <a href="/my-project/quiz">
+            <div class="card">
+             <img src="@/assets/img/FEAD1660-CC3D-4B8B-9DC4-815F0F3901B0.jpeg" class="img-fluid" alt="">
+              <span class="py-2 px-4">Quiz Test</span>
+            </div>
+            </a>
+        </div>
+      </div>
 </template>
 
 <script>
-import LoginComponent from '@/components/LoginComponent.vue'
-
 export default {
     components: {
         
